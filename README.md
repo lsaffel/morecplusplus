a55743f8632554fcbda8f9377ca318a76ec7b90e
Lisa <llvvss@icloud.com>
2024-03-12 15:30:14 -0600

    reverse a number

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 1. Count the number of digits in a number  
    This works for both positive and negative numbers.  
     Limited by the max value that can be stored in an integer variable.
+2. Reversing a number  
+   Takes the number. Removes the last digit. Adds that to  
+   10 times the reversed number in a while loop.  
+   Note that if the original number ends in 0, then  
+   that digit is left off of the reversed number.
--- morecplusplus.cpp
+++ morecplusplus.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 int main()
 {
-    // Count digits of a number
-    int number;
+    // reversing a number
+    int number, reversedNumber = 0;
     cout << "Number: ";
     // cin >> number;
 
-    number = -24689;     // put the number here manually to test this  
-                        // program, since cin doesn't work here in VS Code
+    number = 246810;
 
-    if (number == 0)
-        cout << "You have entered 0.\n";
-    else {
-        if (number < 0)
-            number = -1 * number;   // now able to count digits 
-                    // for negative numbers
-        int digitsCounter = 0;
-        while (number > 0) {
-            // number = number / 10;   // number is an integer so result is also an integer
-            number /= 10;   // shorter way to write the same thing
-            digitsCounter++;
-        }
-        cout << "Number contains " << digitsCounter << " digits\n";
+    cout << number << "  ";
+
+    while (number !=0) {
+        reversedNumber *= 10;
+
+        // now take the last digit of the original number
+        int lastDigit = number % 10;        // mod 10
+        reversedNumber += lastDigit;
+
+        // remove the last digit from the original number
+        number /= 10;
     }
+
+    cout << "Reversed number: " << reversedNumber;
+
 }
